21dd4e64c06dcf58fcbaabc623e01eb89f0eb2f5
Remy <shingiroremy303@gmail.com>
2025-08-14 20:32:17 +0200

    fix: remove duplicate list items and implement drag-and-drop functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+let lists = document.getElementById("list");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+
+for( list of lists) {
+    list.addEventListener("dragstart", function(e) {
+        let selected = e.target;
+
+        rightBox.addEventListener("dragover", function(e) {
+            e.preventDefault();
+        });
+        rightBox.addEventListener("drop", function(e)  {
+                rightBox.appendChild(selected);
+                selected = null;
+        })
+        
+    })
+}
