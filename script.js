ef53f6cb5a46f6b492746929a800eaf5383657b6
Remy <shingiroremy303@gmail.com>
2025-08-15 12:40:14 +0200

    fix: update list selection to use getElementsByClassName for drag-and-drop functionality

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 
-
-let lists = document.getElementById("list");
+let lists = document.getElementsByClassName("list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
 
-for( list of lists) {
-    list.addEventListener("dragstart", function(e) {
+for(list of lists){
+    list.addEventListener("dragstart", function(e){
         let selected = e.target;
 
-        rightBox.addEventListener("dragover", function(e) {
+        rightBox.addEventListener("dragover", function(e){
             e.preventDefault();
         });
-        rightBox.addEventListener("drop", function(e)  {
-                rightBox.appendChild(selected);
-                selected = null;
+        rightBox.addEventListener("drop", function(e){
+            rightBox.appendChild(selected);
+            selected = null;
         })
-        
+
+        leftBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+        leftBox.addEventListener("drop", function(e){
+            leftBox.appendChild(selected);
+            selected = null;
+        })
     })
 }
